2499f394a0a8bafc1d19dc8a71702c6540351a92
VidwaDeSeram <vidwadeseram2002@gmail.com>
2023-04-12 22:36:36 +0530

    attendance history done

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const paymentController = require('../controllers/paymentController');
+const paymentController = require("../controllers/paymentController");
 
-router.post('/', paymentController.createPayment);
+router.post("/", paymentController.createPayment);
 
-router.get('/:studentId', paymentController.getPaymentHistoryByStudentId);
+router.get("/allPaymentHistory", paymentController.getPaymentHistory);
+
+router.get("/:studentId", paymentController.getPaymentHistoryByStudentId);
 
 module.exports = router;
